dc673adccd2a7ea0d08c7ddc74cd0dfbe4de5e1d
Mike Cobbett <77053+techcobweb@users.noreply.github.com>
2024-02-16 10:55:24 +0000

    trying to variableise the image version
    Signed-off-by: Mike Cobbett <77053+techcobweb@users.noreply.github.com>

--- runner.sh
+++ runner.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+echo "GALASACTL_ARGS are : $GALASACTL_ARGS"
 exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" \
     --entrypoint="/bin/galasactl" $DOCKER_OPTIONS \
     icr.io/galasadev/galasa-cli-amd64:$GALASACTL_VERSION \
