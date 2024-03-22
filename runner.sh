1a096b9642e19bca81cad960fcdd306dbbba3d2e
Mike Cobbett <77053+techcobweb@users.noreply.github.com>
2024-03-22 09:47:38 +0000

    use v2 of the github action
    Signed-off-by: Mike Cobbett <77053+techcobweb@users.noreply.github.com>

--- runner.sh
+++ runner.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-echo "INPUT_GALASACTL_ARGS are : $INPUT_GALASACTL_ARGS"
+echo "INPUT_GALASACTL_ARGS are : ${INPUT_GALASACTL-ARGS}"
 exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" \
-    --entrypoint="/bin/galasactl" $INPUT_DOCKER_OPTIONS \
-    icr.io/galasadev/galasa-cli-amd64:$INPUT_GALASACTL_VERSION \
-    "${INPUT_GALASACTL_ARGS}"
+    --entrypoint="/bin/galasactl" ${INPUT_DOCKER-OPTIONS} \
+    icr.io/galasadev/galasa-cli-amd64:${INPUT_GALASACTL-VERSION} \
+    "${INPUT_GALASACTL-ARGS}"
